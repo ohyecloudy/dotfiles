2fc3b1beaaf5c62ced6e82527f28dd9289334580
ohyecloudy <ohyecloudy@gmail.com>
2013-01-24 22:43:22 +0900

    실행 경로를 바탕으로 패스를 계산해 사용 > git root path, symbolic src path

--- script/ios-bootstrap.sh
+++ script/ios-bootstrap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# 실행되는 쉘 절대 경로 구하기
+# http://kldp.org/node/119861
+cd ${0%/*} 2>/dev/null
+shell_path=$PWD/${0##*/}
+
+# TODO: 이거 한번에 하는 방법 없나?
+script_path=${shell_path%/*}
+root_path=${script_path%/*}
+
 echo "submodule init/update"
-cd ~/.vim
+cd $root_path
 git submodule update --init
 
 echo "create symbolic link"
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -s $root_path/vim/vimrc ~/.vimrc
 
