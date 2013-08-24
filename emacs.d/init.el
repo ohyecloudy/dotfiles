9684ac35c3497dafc02ca37b87b3758752b662ef
ohyecloudy <ohyecloudy@gmail.com>
2013-08-24 16:33:23 +0900

    emacs > clojure-mode > 'reindent-then-newline-and-indent에 RET 바인딩 * major mode에서 RET은 줄 바꿈외에 다른 동작을 안 하는 게 표준 * 그래서 clojure-mode 2.0에서 사라졌는데, 너무 불편해 수동 바인딩 * https://groups.google.com/forum/#!topic/clojure/YI4IfthcycU

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -76,6 +76,11 @@
 
 ;; clojure-mode https://github.com/technomancy/clojure-mode
 (require 'clojure-mode)
+; From 23.2.1, GNU Emacs Lisp Reference Manual edition 3.1:
+; Major modes for editing text should not define <RET> to do anything other than insert a newline.
+; 그래서 RET 바인딩이 사라지고 C-j에 바인딩.
+; 하지만 너무 불편해. 그냥 평소처럼 RET에 바인딩해서 사용한다.
+(define-key map (kdb "RET") 'reindent-then-newline-and-indent)
 
 ;; nrepl.el https://github.com/kingtim/nrepl.el
 (require 'nrepl)
