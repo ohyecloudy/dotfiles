f76ff02a37992b2cb4d8924278071789ad0d85a4
ohyecloudy <ohyecloudy@gmail.com>
2014-08-30 23:22:14 +0900

    emacs > require vim-paredit

--- emacs.d/config/vim.el
+++ emacs.d/config/vim.el
@@ -2,6 +2,10 @@
 (require 'evil)
 (evil-mode 1)
 
+;;; evil-paredit
+(add-to-list 'load-path "~/.emacs.d/manual-package/evil-paredit")
+(require 'evil-paredit)
+
 ;; ctrl+u 를 바인딩.
 ;; (setq evil-want-C-u-scroll t)가 동작 안해서 직접 정의
 (define-key evil-motion-state-map (kbd "C-u") 'evil-scroll-up)
