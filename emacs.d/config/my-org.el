3cbb2ff366286055221055df3856e5080710e567
ohyecloudy <ohyecloudy@gmail.com>
2016-09-01 10:29:12 +0900

    emacs > org > plantuml 추가

--- emacs.d/config/my-org.el
+++ emacs.d/config/my-org.el
@@ -6,3 +6,20 @@
 (setq org-journal-time-prefix "")
 
 (setq org-adapt-indentation nil)
+
+;;; image
+(setq org-startup-with-inline-images t)
+
+;;; plantuml
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((plantuml . t)))
+(setq org-confirm-babel-evaluate nil)
+(setq org-plantuml-jar-path
+      (expand-file-name "~/bin/plantuml.jar"))
+(add-hook 'org-babel-after-execute-hook
+          (lambda ()
+            (when org-inline-image-overlays
+              (org-redisplay-inline-images))))
+(add-to-list 'org-structure-template-alist
+             '("u" "#+BEGIN_SRC plantuml :file ?.png\nskinparam monochrome true\n#+END_SRC"))
