0cad274e7951129ff1f20cc10add7d0fcd599609
ohyecloudy <ohyecloudy@gmail.com>
2017-08-15 20:58:25 +0900

    emacs > 80 col 초과 강조 없애고 trailing만 강조

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -360,11 +360,8 @@
 (add-hook
  'after-change-major-mode-hook
  '(lambda ()
-    (if (derived-mode-p 'prog-mode)
-        (setq whitespace-line-column 80
-              whitespace-style '(face trailing lines-tail tab-mark))
-      (setq whitespace-line-column nil
-            whitespace-style '(face trailing tab-mark)))))
+    (setq whitespace-line-column nil
+          whitespace-style '(face trailing))))
 
 ;; disable tabs mode
 (setq-default indent-tabs-mode nil)
