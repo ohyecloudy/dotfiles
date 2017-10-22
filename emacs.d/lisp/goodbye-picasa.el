2a7e1a79f2227581ddc644e8e5df4b2152492513
ohyecloudy <ohyecloudy@gmail.com>
2017-10-22 09:49:12 +0900

    emacs > lisp > goodbye-picasa img fullpath 추출 작업

---
+++ emacs.d/lisp/goodbye-picasa.el
@@ -0,0 +1,82 @@
+(require 'el-mock)
+
+(defun goodbye-picasa (filename)
+  ;; 다운로드할 picasa 주소 추출
+  ;; 해당 주소를 대체할 local path 생성
+  ;; 다운로드
+  ;; 버퍼에서 picasa 주소를 local path로 대체
+  )
+
+;; 이걸 string이나 혹은 file로 바꾸고 싶은데, 가능하나?
+(defun bye-picasa--extract-picasa-images-url (string)
+  (with-temp-buffer
+    (insert string) ; string을 추상화해서 file에서 읽거나 test용 string을 읽게 해야하는 걸까?
+    (goto-char (point-min))
+    (let ((case-fold-search nil)
+          (url-list ()))
+      (while (search-forward-regexp
+              "\\([^][ \"\']*googleusercontent[^][ \"\']*\\)"
+              nil
+              t)
+        (add-to-list 'url-list (match-string 1)))
+      url-list)))
+
+(ert-deftest bye-picasa--extract-picasa-images-url-test ()
+  (let* ((to-extract-1 "https://lh5.googleusercontent.com/-1Txefxmwfdg/TxuFylM_MCI/AAAAAAAAKLA/WANDTRNQdgY/s640/thumbnail-1.jpg")
+         (test-string-1 (concat "[[" to-extract-1 "][test]]"))
+         (test-string-2 (concat "href=\"" to-extract-1 "\">"))
+    (should (equal (bye-picasa--extract-picasa-images-url test-string-1)
+                   (cons to-extract-1 ())))
+    (should (equal (bye-picasa--extract-picasa-images-url test-string-2)
+                   (cons to-extract-1 ()))))))
+
+(defun bye-picasa--make-img-base-path (path)
+  (let ((from "_posts")
+        (to "assets"))
+    (unless (string-match from path)
+      (throw 'path-should-be-contains-a-_posts-directory path))
+    (replace-regexp-in-string
+     from
+     to
+     (concat
+      (file-name-directory path)
+      (file-name-base path)
+      "-"))))
+
+(ert-deftest bye-picasa--make-img-base-path-throw-test ()
+  (should-error (bye-picasa--make-img-base-path "not-contains-_post")
+                :type 'no-catch))
+(ert-deftest bye-picasa--make-img-base-path-test ()
+  (should (equal (bye-picasa--make-img-base-path
+                  "~/project/lifelog/_posts/2009-02-08-523.org")
+                 "~/project/lifelog/assets/2009-02-08-523-")))
+
+(defun bye-picasa--full-img-path (base-path count predicate)
+  (when (> count 99)
+    (throw 'a-count-variable-should-not-exceed-99 count))
+  (let (ret '())
+    (dotimes (i 100)
+      (let ((full-path (concat
+                        base-path
+                        (if (< i 10) "0" "")
+                        (number-to-string i))))
+        (when (funcall predicate full-path)
+          (add-to-list 'ret full-path))))
+    (butlast (reverse ret) (- (length ret) count))))
+
+(ert-deftest bye-picasa--full-img-path-throw-test  ()
+  (should-error (bye-picasa--full-img-path "b-" 100 nil)
+                :type 'no-catch))
+
+(ert-deftest bye-picasa--full-img-path-test ()
+  (should (equal
+           (bye-picasa--full-img-path
+            "b-"
+            3
+            '(lambda (path) (and
+                         (not (string-match "-00" path))
+                         (not (string-match "-02" path)))))
+           (list "b-01" "b-03" "b-04"))))
+
+;;; TODO 이미지 패스에서 확장자 추출하고 full-img-path로 실제 다운로드 받을 이미지 경로 추출
+;;; TODO full-img-path 이건 헷갈린다. 어차피 img 라서. dest와 source path로 구분하자
