aece814f946d8101c2197723c23deacbe3c93ab6
ohyecloudy <ohyecloudy@gmail.com>
2018-03-09 17:34:03 +0900

    emacs > gitlab merge request를 org headline으로 삽입하는 함수
    - 10.6에 updated_before, update_after를 지원할 예정이라 지금은 최근
      100개 삽입

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -780,4 +780,42 @@
                        gitlab-private-key)))
       (insert (format " %s" (parse-title url)))))
 
+  ;; gitlab 10.6에 반영되는지 보자. 아직 updated_before, updated_after는 설명만 반영된 듯
+  ;; https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/17417
+  (defun merge-requests-updated-between-url (begin-date end-date)
+    (let ((before (format "%sT00:00:00.000%%2B09:00" end-date))
+          (after (format "%sT00:00:00.000%%2B09:00" begin-date))
+          (options "order_by=updated_at&state=merged&scope=all&per_page=100"))
+      (format "%s/merge_requests?%s&updated_after=%s&updated_before=%s&private_token=%s"
+              gitlab-api-base-url
+              options
+              after
+              before
+              gitlab-private-key)))
+
+  (defun merge-request-ids-updated-between (begin-date end-date)
+    (let ((ids '())
+          (json-array-type 'list)
+          (url (merge-requests-updated-between-url begin-date end-date)))
+      (with-temp-buffer
+        (url-insert-file-contents url)
+        (let* ((json-key-type 'string)
+               (content (json-read)))
+          (dolist (merge-request content)
+            (dolist (element merge-request)
+              (when (string= (car element) "iid")
+                (add-to-list 'ids (cdr element)))))))
+      ids))
+
+  ;; gitlab 10.6을 기다리자. 그 전엔 update 범위 query 의미없다.
+  ;;(defun insert-gitlab-mrs-between (begin-date end-date)
+  ;;  (interactive "sbegin-date(YYYY-mm-dd): \nsend-date(%s ~ YYYY-MM-DD): ")
+  (defun insert-gitlab-mrs-between ()
+    (interactive)
+    (let ((ids (merge-request-ids-updated-between "2000-01-01" "2000-01-01")))
+      (dolist (id ids)
+        (insert "*** TODO ")
+        (insert-gitlab-mr id)
+        (insert "\n"))))
+
   (message "define gitlab related functions"))
