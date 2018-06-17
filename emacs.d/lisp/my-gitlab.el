2203f1f263e9e5c4b3fd9dda09abcdd204511126
ohyecloudy <ohyecloudy@gmail.com>
2018-06-18 08:30:27 +0900

    emacs > refactor > move functions

--- emacs.d/lisp/my-gitlab.el
+++ emacs.d/lisp/my-gitlab.el
@@ -1,6 +1,77 @@
 (when (and (boundp 'gitlab-private-key)
            (boundp 'gitlab-api-base-url)
            (boundp 'gitlab-base-url))
+  (defun insert-gitlab-mr-link (id)
+    (interactive "nmerge request id: ")
+    (insert (format "[[%s/merge_requests/%d][!%d]]" gitlab-base-url id id)))
+
+  (defun insert-gitlab-issue-link (id)
+    (interactive "nissue id: ")
+    (insert (format "[[%s/issues/%d][#%d]]" gitlab-base-url id id)))
+
+  (defun parse-title (url)
+    (let ((title ""))
+      (with-temp-buffer
+        (url-insert-file-contents url)
+        (let* ((json-key-type 'string)
+               (content (json-read)))
+          (dolist (element content)
+            (when (string= (car element) "title")
+              (setq title (cdr element))))))
+      title))
+
+  (defun insert-gitlab-mr (id)
+    (interactive "nmerge request id: ")
+    (insert-gitlab-mr-link id)
+    (let ((url (format "%s/merge_requests/%d?private_token=%s"
+                       gitlab-api-base-url
+                       id
+                       gitlab-private-key)))
+      (insert (format " %s" (parse-title url)))))
+
+  (defun insert-gitlab-issue (id)
+    (interactive "nissue id: ")
+    (insert-gitlab-issue-link id)
+    (let ((url (format "%s/issues/%d?private_token=%s"
+                       gitlab-api-base-url
+                       id
+                       gitlab-private-key)))
+      (insert (format " %s" (parse-title url)))))
+
+  (defun merge-requests-updated-url (begin-date end-date)
+    (let ((before (format "%sT00:00:00.000%%2B09:00" end-date))
+          (after (format "%sT00:00:00.000%%2B09:00" begin-date))
+          (options "order_by=updated_at&state=merged&scope=all&per_page=100"))
+      (format "%s/merge_requests?%s&updated_after=%s&updated_before=%s&private_token=%s"
+              gitlab-api-base-url
+              options
+              after
+              before
+              gitlab-private-key)))
+
+  (defun merge-request-ids-updated (begin-date end-date)
+    (let ((ids '())
+          (json-array-type 'list)
+          (url (merge-requests-updated-url begin-date end-date)))
+      (with-temp-buffer
+        (url-insert-file-contents url)
+        (let* ((json-key-type 'string)
+               (content (json-read)))
+          (dolist (merge-request content)
+            (dolist (element merge-request)
+              (when (string= (car element) "iid")
+                (add-to-list 'ids (cdr element)))))))
+      ids))
+
+  (defun insert-gitlab-mrs-range ()
+    (interactive)
+    (let* ((begin-date (org-read-date))
+           (end-date (org-read-date))
+           (ids (merge-request-ids-updated begin-date end-date)))
+      (dolist (id ids)
+        (insert "*** TODO ")
+        (insert-gitlab-mr id)
+        (insert "\n"))))
 
   (defun my/commits-url (begin-date end-date page)
     (let ((before (format "%sT00:00:00.000%%2B09:00" end-date))
@@ -69,4 +140,6 @@
         (insert "*** TODO ")
         (insert-gitlab-commit-link (plist-get c :id))
         (insert (format " [%s] %s" (plist-get c :author_name) (plist-get c :title)))
-        (insert "\n")))))
+        (insert "\n"))))
+
+  (message "define gitlab related functions"))
