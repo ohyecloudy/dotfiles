485ba7c6701545123e229dea7ac14d442fded245
ohyecloudy <ohyecloudy@gmail.com>
2019-03-27 16:18:43 +0900

    :cyclone: melpa 패키지 주소 변경

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -71,9 +71,7 @@
 
 ;;; packages
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/")
-             t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
