a9e37bffe44ad954d493a88860fd454b272c49a5
ohyecloudy <ohyecloudy@gmail.com>
2019-04-10 14:48:13 +0900

    :cyclone: CamelCase를 snake_case로 변경하는 함수 추가

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -783,3 +783,20 @@
     (message "Copied default image name '%s' to the clipboard." name)))
 
 (load-file "~/.emacs.d/lisp/my-gitlab.el")
+
+(defun toggle-camelcase-underscores ()
+  "Toggle between camelcase and underscore notation for the symbol at point."
+  (interactive)
+  (save-excursion
+    (let* ((bounds (bounds-of-thing-at-point 'symbol))
+           (start (car bounds))
+           (end (cdr bounds))
+           (currently-using-underscores-p (progn (goto-char start)
+                                                 (re-search-forward "_" end t))))
+      (if currently-using-underscores-p
+          (progn
+            (upcase-initials-region start end)
+            (replace-string "_" "" nil start end)
+            (downcase-region start (1+ start)))
+        (replace-regexp "\\([A-Z]\\)" "_\\1" nil (1+ start) end)
+        (downcase-region start (cdr (bounds-of-thing-at-point 'symbol)))))))
