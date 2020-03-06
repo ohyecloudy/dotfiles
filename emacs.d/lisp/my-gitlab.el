1a9d001c26057cf3939a02828fd812a964f3811e
ohyecloudy <ohyecloudy@gmail.com>
2020-03-06 19:21:47 +0900

    :cyclone::star: 에러 레이블이 붙은 팀원 이슈를 열어주는 함수 추가
    - my-gitlab-open-issue-error 함수 추가
    - assignee가 없는 에러 이슈와 gitlab-team-members 리스트에 있는 사람이
      담당자인 에러 이슈를 임시 버퍼에 org 헤딩으로 이슈를 추가한다

--- emacs.d/lisp/my-gitlab.el
+++ emacs.d/lisp/my-gitlab.el
@@ -2,17 +2,39 @@
 
 (defun my-gitlab-open-todo ()
   (interactive)
+  (when (not (boundp 'gitlab-api-url)) (throw 'gitlab-api-url "not bound"))
   (let ((todos (my-gitlab--request-get (my-gitlab--build-request
+                                        gitlab-api-url
                                         "todos"
                                         '(("per_page" 10))))))
     (dolist (elt todos)
       (browse-url (plist-get elt :target_url)))))
 
-(defun my-gitlab--build-request (page properties)
+(defun my-gitlab-open-issue-error ()
+  (interactive)
+  (when (not (boundp 'gitlab-team-members)) (throw 'gitlab-team-members "not bound"))
+  (switch-to-buffer (make-temp-name "gitlab error issues"))
+  (org-mode)
+  (my-gitlab--insert-issues-by '("assignee_id" "None"))
+  (dolist (elt gitlab-team-members)
+    (message elt)
+    (my-gitlab--insert-issues-by `("assignee_username" ,elt))))
+
+(defun my-gitlab--insert-issues-by (assignee)
+  (let ((issues (my-gitlab--request-get
+                 (my-gitlab--build-request gitlab-api-project-url
+                                           "issues"
+                                           (cons assignee
+                                                 '(("state" "opened")
+                                                   ("labels" "에러")))))))
+    (dolist (elt issues)
+      (org-meta-return)
+      (my-gitlab--insert-issues elt))))
+
+(defun my-gitlab--build-request (api-url page properties)
   (when (not (boundp 'gitlab-private-key)) (throw 'gitlab-private-key "not bound"))
-  (when (not (boundp 'gitlab-api-url)) (throw 'gitlab-api-url "not bound"))
   (format "%s/%s?%s"
-          gitlab-api-url
+          api-url
           page
           (my-gitlab--build-params (cons `("private_token" ,gitlab-private-key) properties))))
 
@@ -25,12 +47,25 @@
     (string-join value "&")))
 
 (defun my-gitlab--request-get (url)
+  (message (format "request get - %s" url))
   (let ((json-array-type 'list)
         (json-object-type 'plist)
         (json-key-type 'keyword))
     (with-temp-buffer
       (url-insert-file-contents url)
       (json-read))))
+
+(defun my-gitlab--insert-issues (issue)
+  (let* ((title (plist-get issue :title))
+         (web_url (plist-get issue :web_url))
+         (iid (plist-get issue :iid))
+         (assignee (or (plist-get issue :assignee) '(:username "none")))
+         (username (plist-get assignee :username)))
+    (insert (format "%s - [%s] [[%s][#%s]]"
+                    title
+                    username
+                    web_url
+                    iid))))
 
 (provide 'my-gitlab)
 ;;; my-gitlab.el ends here
