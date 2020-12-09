a43114c69411bd78bc4e2641590e3f09ae0c2995
ohyecloudy <ohyecloudy@gmail.com>
2020-12-09 11:14:27 +0900

    :cyclone::apple: 깜빡이는 문제(flickering)가 있어서 옵션 추가
    - inhibit-double-buffering

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,8 +18,17 @@
 ;;
 ;; macbook pro에서 full height에 스크린 width 반정도 차지하게 세팅
 ;;
+;; 가끔 깜빡이는 문제가 있어서 inhibit-double-buffering 옵션을 추가함
+;; https://www.reddit.com/r/emacs/comments/ecctcp/how_to_debug_emacs_flickering_issue/
 (when mac?
-  (setq default-frame-alist '((left . 0) (width . 100) (fullscreen . fullheight))))
+  (setq default-frame-alist
+        '(
+          (left . 0) (width . 100)
+          (fullscreen . fullheight)
+          (inhibit-double-buffering . t)
+          )
+        )
+  )
 
 ;; | 12345678 |   |
 ;; |----------+---|
