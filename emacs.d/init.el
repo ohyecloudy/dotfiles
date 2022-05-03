d65350aa9512a486cf53552a4000b3bcdc4a132b
ohyecloudy <ohyecloudy@gmail.com>
2022-05-03 11:18:51 +0900

    :cyclone: org 버퍼에서 링크를 추출해 bullets로 만드는 함수 추가
    - my-org-build-link-section

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1034,20 +1034,62 @@
   (interactive)
   (org-cliplink-insert-transformed-title
    (org-cliplink-clipboard-content)     ;take the URL from the CLIPBOARD
-   (lambda (url title)
-     (let* ((parsed-url (url-generic-parse-url url)) ;parse the url
-            (host-url (replace-regexp-in-string "^www\\." "" (url-host parsed-url)))
-            (clean-title
-             (cond
-              ;; if the host is github.com, cleanup the title
-              ((string= (url-host parsed-url) "github.com")
-               (replace-regexp-in-string "^/" ""
-                                         (car (url-path-and-query parsed-url))))
-              ;; (replace-regexp-in-string "GitHub - .*: \\(.*\\)" "\\1" title))
-              ((string= (url-host parsed-url) "www.youtube.com")
-               (replace-regexp-in-string "\\(.*\\) - Youtube" "\\1" title))
-              ;; otherwise keep the original title
-              (t title)))
-            (title-with-url (format "%s - %s" clean-title host-url)))
-       ;; forward the title to the default org-cliplink transformer
-       (org-cliplink-org-mode-link-transformer url title-with-url)))))
+   #'my-org-link-transformer))
+
+(defun my-org-build-link-section ()
+  (interactive)
+  (let ((links (sort
+                (delete-dups (my-org-extract-urls (org-element-parse-buffer)))
+                'string<)))
+    (org-insert-heading-after-current)
+    (insert "링크")
+    (org-return t)
+    (org-return t)
+    (seq-map-indexed (lambda (elt idx)
+                       (message (format "processing - %s" elt))
+                       (let* ((url (url-encode-url elt))
+                              (title (or (org-cliplink-retrieve-title-synchronously url)
+                                         "nil"))
+                              (link-elt (my-org-link-transformer url title)))
+                         ;; 첫번째 요소는 직접 정렬되지 않은 목록 아이템을 넣어준다
+                         (if (= idx 0)
+                             (progn
+                               (insert (format "- %s" link-elt))
+                               (org-return t))
+                           ;; 두번째 요소 부터는 org-insert-item 함수를 호출해
+                           ;; 이전 목록 아이템을 참고해 자동으로 넣는다
+                           (progn
+                             (org-insert-item)
+                             (insert link-elt)
+                             (org-return t)))))
+                     links)))
+
+(defun my-org-link-transformer (url title)
+  (let* ((parsed-url (url-generic-parse-url url)) ;parse the url
+         (host-url (replace-regexp-in-string "^www\\." "" (url-host parsed-url)))
+         (clean-title
+          (cond
+           ;; if the host is github.com, cleanup the title
+           ((string= (url-host parsed-url) "github.com")
+            (replace-regexp-in-string "^/" ""
+                                      (car (url-path-and-query parsed-url))))
+           ;; (replace-regexp-in-string "GitHub - .*: \\(.*\\)" "\\1" title))
+           ((string= (url-host parsed-url) "www.youtube.com")
+            (replace-regexp-in-string "\\(.*\\) - Youtube" "\\1" title))
+           ;; otherwise keep the original title
+           (t title)))
+         (title-with-url (format "%s - %s" clean-title host-url)))
+    ;; forward the title to the default org-cliplink transformer
+    (org-cliplink-org-mode-link-transformer url title-with-url)))
+
+(defun my-org-extract-urls (org-elements)
+  ;; link 타입 org element만 map
+  (org-element-map org-elements 'link
+    (lambda (link)
+      (let* ((link-part (nth 1 link))
+             (type (plist-get link-part :type))
+             (path (url-unhex-string (plist-get link-part :raw-link))))
+        ;; "https", "http"로 시작하는 link만 골라낸다
+        (if (or (string= type "https") (string= type "http"))
+            ;; "https://...", "http://..." 같은 전체 주소
+            path)))))
