9286fdbe56323b7f652d3c082872ee4679dab66a
ohyecloudy <ohyecloudy@gmail.com>
2022-07-10 14:27:23 +0900

    :cyclone: 옛날 버전을 제거하는 백업 세팅 제거
    - emacs 28.1부터 오동작
      - 중요한 설정이 아니니 제거하고 필요시 문제를 해결해 활성화한다

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -573,10 +573,6 @@
 
 ;;; backup
 (add-to-list 'backup-directory-alist '("." . "~/.emacs-saves"))
-(setq delete-old-versions t
-      kept-old-versions 2
-      kept-new-versions 2
-      version-control t)
 
 ;; whitespace mode
 (custom-set-faces
