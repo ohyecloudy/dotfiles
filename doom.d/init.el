6b9e429a215772f34cb645768e1839ec13849bc0
ohyecloudy <ohyecloudy@gmail.com>
2022-09-25 12:34:18 +0900

    Revert ":cyclone: doom: 밀고 있는 vertico로 변경"
    This reverts commit f291b8d39cd4eb2a03582c11485f0f377704aa7f.

--- doom.d/init.el
+++ doom.d/init.el
@@ -22,10 +22,10 @@
 
        :completion
        company           ; the ultimate code completion backend
-       ;;(helm +fuzzy)     ; the *other* search engine for love and life
+       (helm +fuzzy)     ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       ;;vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
