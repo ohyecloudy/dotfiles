bf658f35f904ce7b962c73faf6cb6c9687afd8d0
ohyecloudy <ohyecloudy@gmail.com>
2022-11-09 15:56:08 +0900

    :cyclone: doom rest module 사용
    있는지 모르고 직접 정의해서 사용했다

--- doom.d/init.el
+++ doom.d/init.el
@@ -161,7 +161,7 @@
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
-       ;;rest              ; Emacs as a REST client
+       rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
