40b4031d83c58b9e1834fd94d1805d2343b01d3b
ohyecloudy <ohyecloudy@gmail.com>
2024-03-19 16:20:24 +0900

    :cyclone: vertico, syntax에 childframe 추가

--- doom.d/init.el
+++ doom.d/init.el
@@ -25,7 +25,7 @@
        ;;(helm +fuzzy)     ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +icons)    ; the search engine of the future
+       (vertico +childframe +icons)    ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -81,7 +81,7 @@
        (:if IS-MAC vterm)  ; the best terminal emulation in Emacs
 
        :checkers
-       syntax              ; tasing you for every semicolon you forget
+       (syntax +childframe) ; tasing you for every semicolon you forget
        ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
