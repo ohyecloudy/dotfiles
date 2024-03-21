0174fa486c59e0b8ab32d5a9f655237d9a84e58a
ohyecloudy <ohyecloudy@gmail.com>
2024-03-21 15:13:05 +0900

    :cyclone: vertico childframe 제거
    중앙에 떠서 예쁘긴 한데, 크게 변화도 그렇고 사용 중인 버퍼를 가려서
    불편함

--- doom.d/init.el
+++ doom.d/init.el
@@ -25,7 +25,7 @@
        ;;(helm +fuzzy)     ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +childframe +icons)    ; the search engine of the future
+       (vertico +icons)    ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
