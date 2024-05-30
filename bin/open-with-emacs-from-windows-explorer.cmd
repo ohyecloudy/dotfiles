16289143568ee0662505dea1a9fc7a6202360839
ohyecloudy <ohyecloudy@gmail.com>
2024-05-30 15:32:17 +0900

    feat(windows): emacs로 열 때 CUR_EMACS_BIN_DIR 환경 변수
    - REG_SZ를 REG_EXPAND_SZ로 변경
      - 환경 변수를 사용해서 문자 그대로가 아니라 환경 변수로 대체하려면
        확장 가능한 문자열을 사용해야 함

--- bin/open-with-emacs-from-windows-explorer.cmd
+++ bin/open-with-emacs-from-windows-explorer.cmd
@@ -1,19 +1,24 @@
 @REM https://gist.github.com/roundand/9367852
 @chcp 65001
 
+SET CUR_EMACS_BIN_DIR
+if "%CUR_EMACS_BIN_DIR%"=="" (
+    echo "Cannot find `CUR_EMACS_BIN_DIR` environment variable"
+    set errorlevel=4
+    goto :finally
+)
+
 call is-elevated.bat || goto :finally
 
-SET EMACS_DIR
-if "%EMACS_DIR%"=="" set EMACS_DIR=C:\emacs\bin
-SET ecPath=%EMACS_DIR%\emacsclientw.exe
+SET ecPath=%%CUR_EMACS_BIN_DIR%%\emacsclientw.exe
 
 REG ADD "HKCR\*\shell\Open with Emacs"         /t REG_SZ /v "" /d "Open with Emacs" /f || goto :finally
 REG ADD "HKCR\*\shell\Open with Emacs"         /t REG_EXPAND_SZ /v "Icon" /d "%ecPath%,0" /f || goto :finally
-REG ADD "HKCR\*\shell\Open with Emacs\command" /t REG_SZ /v "" /d "%ecPath% \"%%1\"" /f || goto :finally
+REG ADD "HKCR\*\shell\Open with Emacs\command" /t REG_EXPAND_SZ /v "" /d "%ecPath% \"%%1\"" /f || goto :finally
 
 REG ADD "HKCR\Folder\shell\Open with Emacs"         /t REG_SZ /v "" /d "Open with Emacs" /f || goto :finally
 REG ADD "HKCR\Folder\shell\Open with Emacs"         /t REG_EXPAND_SZ /v "Icon" /d "%ecPath%,0" /f || goto :finally
-REG ADD "HKCR\Folder\shell\Open with Emacs\command" /t REG_SZ /v "" /d "%ecPath% \"%%1\"" /f || goto :finally
+REG ADD "HKCR\Folder\shell\Open with Emacs\command" /t REG_EXPAND_SZ /v "" /d "%ecPath% \"%%1\"" /f || goto :finally
 
 :finally
 @set err=%errorlevel%
