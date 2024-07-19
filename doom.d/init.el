9edf0d2e7d5716970976093428120ede359224ca
ohyecloudy <ohyecloudy@gmail.com>
2024-07-19 10:18:21 +0900

    feat(emacs): enable +lsp flags for future

--- doom.d/init.el
+++ doom.d/init.el
@@ -61,7 +61,7 @@
        ;; org-roam-node-insert 에서 발생하는 오류 때문에 주석 처리
        ;;file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       (format +onsave +lsp)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
