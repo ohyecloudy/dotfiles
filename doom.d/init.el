6a1231bfd19f03eb13b8fcda917c011734e3b5f3
ohyecloudy <ohyecloudy@gmail.com>
2025-04-09 10:38:18 +0900

    feat(emacs): use racket instead of guile scheme

--- doom.d/init.el
+++ doom.d/init.el
@@ -171,7 +171,7 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme +guile)     ; a fully conniving family of lisps
+       (scheme +racket)    ; a fully conniving family of lisps
        (sh +lsp +powershell +tree-sitter) ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
