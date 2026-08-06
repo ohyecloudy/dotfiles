61360724166598d445e6a2b53600acd352dc4ce6
ohyecloudy <ohyecloudy@gmail.com>
2026-08-06 09:57:02 +0900

    feat(emacs): use tj3.bat on windows
    - tj3 사용시 $0 경로 해석이 의도대로 안 돼서 LoadError 발생

--- doom.d/lisp/taskjuggler-setting.el
+++ doom.d/lisp/taskjuggler-setting.el
@@ -31,3 +31,10 @@ resourcereport resourceGraph \"\" {
   hidetask ~(isleaf() & isleaf_())
   sorttasks plan.start.up
 }"))
+
+;; Windows + git-sdk bash 환경에서 `tj3`(sh polyglot wrapper)는
+;; 네이티브 ruby.exe에 POSIX 경로($0)를 넘겨 LoadError 발생.
+;; `tj3.bat`은 Windows 절대경로를 넘겨 정상 동작하므로 이를 사용.
+(when (eq system-type 'windows-nt)
+  (setq org-taskjuggler-process-command
+        "tj3.bat --silent --no-color --output-dir %o %f"))
